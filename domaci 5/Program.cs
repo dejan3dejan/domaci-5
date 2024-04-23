10303e96cadecc19ddb250cee6e93808221683d3
dejan3dejan <154616704+dejan3dejan@users.noreply.github.com>
2024-04-23 18:14:04 +0200

    Update Program.cs

--- domaci 5/Program.cs
+++ domaci 5/Program.cs
@@ -1,20 +1,24 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main(string[] args)
     {
         int[,] unetaMatrica = unesiMatricu();
-        (int minValue, int minRow, int minCol) = minElement(unetaMatrica);
-        Console.WriteLine("Najmanji broj u matrici je {0} i nalazi se na poziciji [{1},{2}]", minValue, minRow, minCol);
+        if (unetaMatrica != null)
+        {
+            (int min, int minRed, int minKol) = minElement(unetaMatrica);
+            Console.WriteLine("Najmanji broj u matrici je {0} i nalazi se na poziciji [{1},{2}]", min, minRed, minKol);
+        }
+        Console.WriteLine("Pritisnite bilo koji taster za izlaz...");
         Console.ReadKey();
     }
 
     static (int, int, int) minElement(int[,] matrica)
     {
         int min = matrica[0, 0];
-        int minRow = 0;
-        int minCol = 0;
+        int minRed = 0;
+        int minKol = 0;
 
         for (int i = 0; i < matrica.GetLength(0); i++)
         {
@@ -23,28 +27,49 @@ class Program
                 if (min > matrica[i, j])
                 {
                     min = matrica[i, j];
-                    minRow = i;
-                    minCol = j;
+                    minRed = i;
+                    minKol = j;
                 }
             }
         }
 
-        return (min, minRow, minCol);
+        return (min, minRed, minKol);
     }
 
     static int[,] unesiMatricu()
     {
-        Console.WriteLine("Unesite broj redova i kolona matrice:");
-        int red = Convert.ToInt32(Console.ReadLine());
-        int kol = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Unesite broj redova matrice:");
+        int red;
+        if (!int.TryParse(Console.ReadLine(), out red) || red <= 0)
+        {
+            Console.WriteLine("Neispravan unos za broj redova. Molimo unesite pozitivan ceo broj.");
+            return null;
+        }
+
+        Console.WriteLine("Unesite broj kolona matrice:");
+        int kol;
+        if (!int.TryParse(Console.ReadLine(), out kol) || kol <= 0)
+        {
+            Console.WriteLine("Neispravan unos za broj kolona. Molimo unesite pozitivan ceo broj.");
+            return null;
+        }
+
         int[,] matrica = new int[red, kol];
 
         Console.WriteLine("Unesite vrednosti:");
+
         for (int i = 0; i < red; i++)
         {
             for (int j = 0; j < kol; j++)
             {
-                matrica[i, j] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Unesite element [{0},{1}]:", i, j);
+                int element;
+                if (!int.TryParse(Console.ReadLine(), out element))
+                {
+                    Console.WriteLine("Neispravan unos. Molimo unesite ceo broj.");
+                    return null;
+                }
+                matrica[i, j] = element;
             }
         }
 
